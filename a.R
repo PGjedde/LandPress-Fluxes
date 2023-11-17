34932f8cd7d4a0a0b15b3821a9980566b789e990
Philip Gjedde <99658792+PGjedde@users.noreply.github.com>
2023-11-17 11:33:30 +0100

    Update a.R

--- a.R
+++ a.R
@@ -1,3 +1,14 @@
 library(tidyverse)
 library(ggplot2)
 library(vegan)
+# Uhhhh, I love the fact that there is a "vegan" package! Does it cook vegan food for me?
+# I don't have a cd-reader, so would the food come out the usb-connection? Is it then kind of a paste?
+
+
+
+cook_vegan_pancakes <- function(){
+  print("Mix 1 large mature banana with 2 tbsp sugar, a pinch of salt, 120g flour, 1/2 tbsp baking powder, and 150ml oatmilk. Fry on a medium heated pan in a bit of oil or margerine. Serve with sliced fruits and a bit of syrup. Enjoy <3")
+
+}
+
+# Do you like banana pancakes?
