d99570c906fbc6a8537b6a0bdcae45f9c5dab202
Philip G <philip.gjedde@ntnu.no>
2023-11-17 11:04:12 +0100

    Added not funny text, but valid questions.

--- a.R
+++ a.R
@@ -1,3 +1,7 @@
 library(tidyverse)
 library(ggplot2)
 library(vegan)
+
+
+# Uhhhh, I love the fact that there is a "vegan" package! Does it cook vegan food for me?
+# I don't have a cd-reader, so would the food come out the usb-connection? Is it then kind of a paste?
